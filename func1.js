6ac9964c81960f2211daf262f1dffca8bdd2ef98
Renatho <renatho@gmail.com>
2017-04-01 08:04:00 -0300

    func test

--- func1.js
+++ func1.js
@@ -1,8 +1,6 @@
 var func2 = require('./func2');
 
-var func1 = function() {
+module.exports = function() {
   console.log('func2 on func1:', func2);
   func2();
 };
-
-module.exports = func1;
--- func3.js
+++ func3.js
@@ -1,7 +1,5 @@
 var func2 = require('./func2');
 
-var func3 = function() {
+module.exports = function() {
   console.log('func2 on func3: ', func2);
 };
-
-module.exports = func3;
